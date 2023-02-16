76a7c7f3c5b32306688c23dc15f620ba98a64c75
Michael Mount <mount.photos@gmail.com>
2023-02-15 20:27:39 -0700

    randome question displayed

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -3,26 +3,47 @@ var titleCard = document.querySelector(".titleCard");
 var gameCard = document.querySelector(".gameCard");
 var scoreBoard = document.querySelector(".scoreBoard");
 var startBtn = document.querySelector(".st-btn");
+var questionEl = document.querySelector(".gameCard");
+var questionTitle = document.querySelector("#title");
+var optionBtns = document.querySelectorAll(".option");
 
 var wins = 0;
 var loses = 0;
 
 var questions = [
   {
-    question: "What Color is the sky?",
+    title: "What Color is the sky?",
     options: ["Blue", "Red", "Green", "Yellow"],
     right: "Blue",
   },
+  {
+    title: "What is 2+2?",
+    options: ["4", "5", "6", "7"],
+    right: "4",
+  },
+  {
+    title: "What is written on the Ceiling",
+    options: ["Gulllable", "Hi", "im tired", "ahhhhh"],
+    right: "Gulllable",
+  },
 ];
 
-console.log(titleCard.dataset.state);
-//Functions
+//FUNCTIONS
 
 function startGame() {
   titleCard.setAttribute("class", "titleCard hidden");
   gameCard.setAttribute("class", "gameCard shown");
-
-  
-
+  nextQuestion();
 }
+
+function nextQuestion() {
+  var randomQuestion = Math.floor(Math.random() * questions.length);
+
+  questionTitle.textContent = questions[randomQuestion].title;
+
+  for (var i = 0; i < questions[randomQuestion].options.length; i++) {
+    optionBtns[i].textContent = questions[randomQuestion].options[i]; //iterates over options array and adds options index value
+  }
+}
+
 startBtn.addEventListener("click", startGame);
