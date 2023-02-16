5d51284ad00283f2ec984b9b0a9798dc7b577abd
Michael Mount <mount.photos@gmail.com>
2023-02-15 17:30:28 -0700

    Adding array

---
+++ assets/Javascript/script.js
@@ -0,0 +1,27 @@
+//Variable Declrations in Global Scope
+var titleEL = document.querySelector(".title");
+var  startBtn = document.querySelector("#Start");
+var questions = [
+  {
+    title: "What Color is the sky?",
+    options: ["Blue", "Red", "Green", "Yellow"],
+    correct: 0,
+  },
+  {
+    title: "What Color is the grass?",
+    options: ["Blue", "Red", "Green", "Yellow"],
+    correct: 2,
+  },
+  {
+    title: "What Color is the sun?",
+    options: ["Blue", "Red", "Green", "Yellow"],
+    correct: 3,
+  },
+];
+
+function showQuestion() {}
+
+startBtn.addEventListener("click", function(){
+  
+})
+
