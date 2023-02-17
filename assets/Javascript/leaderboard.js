e4bafcef43e738bff9c1af338a6c9ab29360594f
Michael Mount <mount.photos@gmail.com>
2023-02-16 21:32:19 -0700

    Added css to LeaderBoard Page

--- assets/Javascript/leaderboard.js
+++ assets/Javascript/leaderboard.js
@@ -1,3 +1,10 @@
-var score = localStorage.getItem.valueof("score");
+var displayScore = JSON.parse(localStorage.getItem("highScores"));
+console.log(displayScore);
 
-console.log(score);
+for (var i = 0; i < displayScore.length; i++) {
+  var display = document.getElementById("leaderboard");
+  var liEl = document.createElement("li");
+
+  liEl.textContent = displayScore[i].initals + ": " + displayScore[i].score;
+  display.appendChild(liEl);
+}
